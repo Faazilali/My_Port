5c17a802a18b91c19e7d47aedc7f78205f890383
Poonawala Faazil Ali <139684678+Faazilali@users.noreply.github.com>
2024-08-01 22:43:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openBar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+
+function closeBar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
+
